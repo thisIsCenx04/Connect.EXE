4c041fa6257c6d2dfefd80eb0bfb7ea7356d8ff5
anlttce182383 <anlttce182383@fpt.edu.vn>
2026-01-07 15:49:32 +0700

    initializing project

---
+++ backend/src/main/java/com/connectexe/edu/ConnectExeApplication.java
@@ -0,0 +1,13 @@
+package com.connectexe.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectExeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/connectexe/edu/ConnectExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectexe.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
